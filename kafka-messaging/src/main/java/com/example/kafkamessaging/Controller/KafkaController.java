e23084acec6c06a9e4ddb0e33dee8385d0debc1c
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2024-03-14 16:21:15 +0000

    Fixing consumer issues + Adding data record

--- kafka-messaging/src/main/java/com/example/kafkamessaging/Controller/KafkaController.java
+++ kafka-messaging/src/main/java/com/example/kafkamessaging/Controller/KafkaController.java
@@ -1,19 +1,25 @@
 package com.example.kafkamessaging.Controller;
 
+import com.example.kafkamessaging.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/kafka")
+@RequestMapping("api/kafka/send")
 public class KafkaController {
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void publish(@RequestBody String message) {
-        kafkaTemplate.send("sporterz", message);
+    public KafkaController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void publish(@RequestBody Message message) {
+        kafkaTemplate.send("sporterz", message.mssg());
     }
 }
