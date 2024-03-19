537cb5d591c900736354ff15ac2d65884ed86e2c
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2024-03-19 22:23:35 +0000

    Implementing Microservices Architecture

---
+++ messaging-service/src/main/java/com/example/kafkamessaging/Controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.example.kafkamessaging.Controller;
+
+import com.example.kafkamessaging.MessageRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/kafka/send")
+public class KafkaController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void publish(@RequestBody MessageRecord messageRecord) {
+        kafkaTemplate.send("sporterz", messageRecord.message());
+    }
+}
---
+++ notification-service/src/main/java/com/sporterz/App.java
@@ -0,0 +1,13 @@
+package com.sporterz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
