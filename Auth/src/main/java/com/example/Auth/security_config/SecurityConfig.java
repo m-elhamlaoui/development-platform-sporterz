5b73565cc186415786f1f5e20bbe74cbddddb67c
blacktache <adnanemandili@gmail.com>
2024-04-28 11:50:40 +0100

    modified back-end with deleting Oauth service

--- Auth/src/main/java/com/example/Auth/security_config/SecurityConfig.java
+++ Auth/src/main/java/com/example/Auth/security_config/SecurityConfig.java
@@ -35,8 +35,6 @@ public class SecurityConfig {
                  .anyRequest()
                  .authenticated()
                  .and()
-                 .oauth2Login()
-                 .and()
                  .sessionManagement()
                  .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                  .and()
